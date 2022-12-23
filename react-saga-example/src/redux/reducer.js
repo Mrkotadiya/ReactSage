494e0702a1e39b19fde32ac9a7edd2b42a980b7c
Mrkotadiya <nirbhayk7281@gmail.com>
2022-12-23 12:30:19 +0530

    add Remove From cart

--- react-saga-example/src/redux/reducer.js
+++ react-saga-example/src/redux/reducer.js
@@ -1,21 +1,24 @@
 
 export const cartData = (data = [], action) => {
-  console.log("reducer called",action);
-
-
-  switch(action.type){
-      case "ADD_TO_CART":
+    
+    switch(action.type){
+        case "ADD_TO_CART":
+          console.log("ADD_TO_CART condition called",action);
           return [
               action.data,
               ...data
           ];
-      case "REMOVE_TO_CART":
 
-        data.length? data.length = data.length-1:data=[];
+      case "REMOVE_TO_CART":
+        console.log("REMOVE_TO_CART condition called",action);
+        // data.length? data.length = data.length-1:data=[];
+        const remainingItems= data.filter((item)=>item.id!==action.data);
+        console.warn("remaining items here ", remainingItems);
             return [
-                ...data
+                ...remainingItems
             ];
       case "EMPTY_CART":
+        console.log("EMPTY_CART condition called",action);
         data=[];
             return [
                 ...data
